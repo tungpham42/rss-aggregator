4a25301bf86b6e9c9e9918504c1ee8335a8d48dd
Tung Pham <tung.42@gmail.com>
2024-07-21 10:40:38 +0700

    Update fetch_rss.php

--- fetch_rss.php
+++ fetch_rss.php
@@ -7,6 +7,10 @@ function removeCdataTags($input) {
     return str_replace(["<![CDATA[", "]]>"], "", $input);
 }
 
+function isGravatarUrl($url) {
+    return strpos($url, 'gravatar.com') !== false;
+}
+
 function fetchRSS($url) {
     $rss = simplexml_load_file($url);
     $items = [];
@@ -29,13 +33,21 @@ function fetchRSS($url) {
             $image_url = '/rss_noimg.png';
             if (isset($item->enclosure)) {
                 $image_url = (string) $item->enclosure->attributes()->url;
-            } elseif ($item->children('media', true)->content) {
+            } elseif ($item->children('media', true)->content && !preg_match('/<img[^>]+src=["\']([^">]+)["\']/i', $item->description, $matches)) {
                 $image_url = (string) $item->children('media', true)->content->attributes()->url;
-            } elseif ($item->children('media', true)->thumbnail) {
+                if (isGravatarUrl($image_url)) {
+                    foreach ($item->children('media', true)->content as $mediaContent) {
+                        $url = (string) $mediaContent->attributes()->url;
+                        if (!isGravatarUrl($url)) {
+                            $image_url = $url;
+                        } else {
+                            $image_url = '/rss_noimg.png';
+                        }
+                    }
+                }
+            } elseif ($item->children('media', true)->thumbnail && !preg_match('/<img[^>]+src=["\']([^">]+)["\']/i', $item->description, $matches)) {
                 $image_url = (string) $item->children('media', true)->thumbnail->attributes()->url;
-            } elseif (preg_match('/<img[^>]+src="([^">]+)"/i', $item->description, $matches)) {
-                $image_url = $matches[1];
-            } elseif (preg_match('/<img[^>]+src=\'([^">]+)\'/i', $item->description, $matches)) {
+            } elseif (preg_match('/<img[^>]+src=["\']([^">]+)["\']/i', $item->description, $matches)) {
                 $image_url = $matches[1];
             }
 
@@ -64,9 +76,7 @@ function fetchRSS($url) {
             
             // Extract image URL if available
             $image_url = '/rss_noimg.png';
-            if (preg_match('/<img[^>]+src="([^">]+)"/i', $entry->summary, $matches)) {
-                $image_url = $matches[1];
-            } elseif (preg_match('/<img[^>]+src=\'([^">]+)\'/i', $entry->summary, $matches)) {
+            if (preg_match('/<img[^>]+src=["\']([^">]+)["\']/i', $entry->summary, $matches)) {
                 $image_url = $matches[1];
             }
 
